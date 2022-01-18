18dfb651bdfb86aec442044c53f06b81959dfbff
Vitaly1805 <vitalinosov2000@gmail.com>
2022-01-18 21:20:16 +0300

    finished layout the header

--- src/js/modules/script.js
+++ src/js/modules/script.js
@@ -8,15 +8,21 @@ window.onload = function () {
     function documentActions(e) {
         const targetElement = e.target;
 
-        if(window.innerWidth > 768 && isMobile.any()) {
-            if(targetElement.classList.contains('menu__arrow-svg')) {
-                targetElement.closest('.menu__item').classList.toggle("_hover");
-            } else if(document.querySelector("._hover")) {
-                const clearHover =  document.querySelectorAll("._hover");
-                clearHover.forEach((e) => {
-                    e.classList.toggle("_hover");
-                })
-            }
+        if(targetElement.classList.contains('menu__arrow-svg')) {
+            targetElement.closest('.menu__item').classList.toggle("_hover");
+        } else if(targetElement.classList.contains('menu-icon')) {
+            const menuIcons = document.querySelectorAll('.menu-icon');
+            const menuBody = document.querySelector('.menu__body');
+
+            menuIcons[0].classList.toggle("menu-open");
+            menuIcons[1].classList.toggle("menu-close");
+            menuBody.classList.toggle("_active");
+        } 
+        else if(document.querySelector("._hover")) {
+            const clearHover =  document.querySelectorAll("._hover");
+            clearHover.forEach((e) => {
+                e.classList.toggle("_hover");
+            })
         }
     }
 }
